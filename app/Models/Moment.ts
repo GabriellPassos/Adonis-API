c36b97cce07e9c9c4597a9db1efcc9b0251dcdce
Gabriel Passos <gabrielsilvapassos42@gmail.com>
2023-04-26 21:35:34 -0300

    migracao modelo moments

--- app/Models/Moment.ts
+++ app/Models/Moment.ts
@@ -4,7 +4,12 @@ import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 export default class Moment extends BaseModel {
   @column({ isPrimary: true })
   public id: number
-
+  @column()
+  public title: string
+  @column()
+  public description: string
+  @column()
+  public image: string
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
